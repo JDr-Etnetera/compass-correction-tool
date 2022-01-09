6f47e25ac9daf6a6c40fd3580acf8808aae09323
JDr-Etnetera <56111669+JDr-Etnetera@users.noreply.github.com>
2022-01-09 19:05:58 +0100

    ongoing save, just for sure

--- main.ts
+++ main.ts
@@ -22,20 +22,32 @@ class CorrectionMap {
     private map: number[][]; // angle index - values count
     private angleList: number[];
     constructor(keys: number, inRow: number) {
+        //unused right now
         this.maxKeys = keys;
+        // rename to maxEntities
         this.maxInRow = inRow;        
         this.reset();
     }
     private isNewAngle(angle: number) {
         return ! (this.angleList.indexOf(angle) >= 0)
     }
-    addAngle(angle: number) {
+    addAngle(angle: number) {        
         if (this.isNewAngle(angle)) {
             this.map[angle] = [];
             this.map[angle][0] = 0;
             this.angleList.push(angle);
         }
         this.map[angle][0] = this.map[angle][0] + 1;
+    }
+    removeAngle(angle: number) {
+        if (this.isNewAngle(angle)) 
+            return false;
+        if (this.map[angle][0] < 1) {
+            this.map[angle] = []
+            return true;
+        } 
+        this.map[angle][0] = this.map[angle][0] - 1;
+        return true;
     }
     removeAngleHistory(angle: number) {
         this.map[angle][0] = 0;
@@ -56,45 +68,46 @@ class CorrectionMap {
     }
     */ 
 }
-class LimitedFront {
+class CompassHistory {
     private maxEntries: number;
-    private front: number[];
+    private history: number[];
     private correction: CorrectionMap;
     constructor(max: number) {
-        this.front = [];
+        this.history = [];
         this.maxEntries = max;
         this.correction = new CorrectionMap(10, 20);
     }
-    append(value: number) {
-        if (this.front.length >= this.maxEntries) {
-            this.front.splice(0, this.front.length - this.maxEntries +1);
+    append(angle: number) {
+        if (this.history.length >= this.maxEntries) {
+            this.history.splice(0, this.history.length - this.maxEntries +1);
+            this.correction.removeAngle(angle);
         }
-        this.front.push(value);
-        this.correction.addAngle(value);
+        this.history.push(angle);
+        this.correction.addAngle(angle);
     }
     getDelta(dec: number) {
         if (this.getLength() < 1) return -100;
         let sum = 0;
-        this.front.forEach(function (e) {
+        this.history.forEach(function (e) {
             sum += e;
         });
         return Math.round( (sum * Math.pow(10, dec)) / this.getLength() ) / Math.pow(10, dec);
     }
     getFromRow() {
-        if (this.front.length > 0) {
-            return this.front.slice(0,1);
+        if (this.history.length > 0) {
+            return this.history.slice(0,1);
         }
         return -100;
     }
     getLength() {
-        return this.front.length;
+        return this.history.length;
     }
     reset() {
-        this.front = [];
+        this.history = [];
         this.correction.reset();
     }
     getHistory() {
-        return this.front
+        return this.history
     }
     getCorrection() {
         return this.correction.getAll();
@@ -103,7 +116,7 @@ class LimitedFront {
         return this.correction.getKnownAngles();
     }
 }
-let cHistory = new LimitedFront(100);
+let cHistory = new CompassHistory(100);
 loops.everyInterval(1000, function () {
     tmp = getDelta_ComHis()
     if (tmp < 0) {
